07ac07b7a6ec934c8e882b03f6e25bc85b535e3d
Ken Kawaguchi <ktkawaguchi@cpp.edu>
2019-02-01 23:22:44 -0800

    Added working logic, need to flesh out the CSS

--- javascript.js
+++ javascript.js
@@ -1,7 +1,29 @@
+var member1 = "http://www.google.com";
+var member2 = "https://www.cpp.edu/";
+var member3 = "http://yahoo.com";
+var link = document.createElement("a");
+
 //create event handler for button and fires function
 document.getElementById("printInfo").onclick = function() {
-    console.log("is this working?");
-    var textInput = document.getElementById("textbox").nodeValue;
-    console.log(textInput);
-    document.getElementById("results").innerHTML = "Just change";
+    var textInput = document.getElementById("textbox").value;
+    var numberInput = document.getElementById("numberbox").value;
+    console.log(textInput + " " + numberInput);
+    document.getElementById("results").innerHTML = textInput + " " + numberInput;
+    linkLogic(numberInput);
+};
+
+//logic for deciding what link to create and creates it
+function linkLogic (num) {
+    if (num < 0) {
+        console.log(member1);
+        link.href = member1;
+    } else if (num >= 0 && num < 30) {
+        console.log(member2);
+        link.href = member2;
+    } else {
+        console.log(member3);
+        link.href = member3;
+    }
+    link.textContent = "Members Link";
+    document.getElementById("results").appendChild(link);
 };
